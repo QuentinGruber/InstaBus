da45133db6ee51e24c7550b259f734b88511030d
Quentin Gruber <quentingruber@gmail.com>
2021-01-09 16:36:02 +0100

    Create Station.kt

---
+++ app/src/main/java/com/example/instabus/interfaces/Station.kt
@@ -0,0 +1,14 @@
+package com.example.instabus.interfaces
+
+interface Station {
+    val id: String
+    val street_name: String
+    val city: String
+    val utm_x: String
+    val utm_y: String
+    val lat: String
+    val lon: String
+    val furniture: String
+    val buses: String
+    val distance: String
+}
