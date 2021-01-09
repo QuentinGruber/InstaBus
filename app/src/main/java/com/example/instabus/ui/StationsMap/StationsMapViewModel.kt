5b2fc9a4b08faab519839d5904fd25df11bc41ea
Quentin Gruber <quentingruber@gmail.com>
2021-01-09 21:07:41 +0100

    remove usell fragment for google map

--- app/src/main/java/com/example/instabus/ui/StationsMap/StationsMapViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.instabus.ui.StationsMap
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class StationsMapViewModel : ViewModel() {
-
-}
