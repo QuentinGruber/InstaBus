459d6e7ddf1980d53e57e510ca3a08509950fc6b
Quentin Gruber <quentingruber@gmail.com>
2021-01-09 20:36:26 +0100

    remove Dummy content

--- app/src/main/java/com/example/instabus/StationListGenFragment.kt
+++ app/src/main/java/com/example/instabus/StationListGenFragment.kt
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.instabus.dummy.DummyContent
 import com.example.instabus.MainActivity
 import com.example.instabus.MainActivity.Companion.Stations
 
