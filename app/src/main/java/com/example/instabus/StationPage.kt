cc078f4a794ca8a199fff2a50cff77a7883212f5
Quentin Gruber <quentingruber@gmail.com>
2021-01-18 14:31:30 +0100

    working camera

--- app/src/main/java/com/example/instabus/StationPage.kt
+++ app/src/main/java/com/example/instabus/StationPage.kt
@@ -1,12 +1,22 @@
 package com.example.instabus
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.tv.TvContract.AUTHORITY
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 
 class StationPage : AppCompatActivity() {
@@ -24,10 +34,22 @@ class StationPage : AppCompatActivity() {
             t1.text = stationName
         }
 
-
+        val REQUEST_CODE = 42;
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            ActivityCompat.requestPermissions(this, Array<String>(1) { Manifest.permission.CAMERA }, REQUEST_CODE);
+           // val f = File(Environment.getExternalStorageDirectory(), "temp.jpg")
+
+            val takePictureintent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+           // takePictureintent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this@StationPage, AUTHORITY, f));
+            //takePictureintent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
+            {
+                Toast.makeText(this, "Unable to open camera reason : PERMISSION_DENIED", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                startActivityForResult(takePictureintent, REQUEST_CODE)
+            }
         }
     }
 }
